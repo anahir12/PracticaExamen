8a856b5cbc9d627c29a12cca998ee153ee4bc182
Anahi Reyes <axreyes0@gmail.com>
2016-06-06 14:58:33 -0600

    Tarea Calculadora
    Terminamos.

--- practicaexamen/routes/index.js
+++ practicaexamen/routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/Calculadora', function(req, res, next) {
+  res.render('Calculadora', { });
+});
+
+router.post('/Calculadora', function(req, res, next) {
+  req.body.Suma= parseFloat(req.body.txtNumero1) + parseFloat(req.body.txtNumero2);
+  req.body.Resta= parseFloat(req.body.txtNumero1) - parseFloat(req.body.txtNumero2);
+  req.body.Multi= parseFloat(req.body.txtNumero1) * parseFloat(req.body.txtNumero2);
+  req.body.Division= parseFloat(req.body.txtNumero1) / parseFloat(req.body.txtNumero2);
+  res.render('Calculadora', req.body);
+});
+
+
 module.exports = router;
